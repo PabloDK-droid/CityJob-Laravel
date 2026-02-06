2a06cc30572ce26acfe5e9f288ca306c26a64ccd
Pablo Sanz <pasabus45@gmail.com>
2026-02-06 00:26:41 -0600

    Primera version de proyecto CityJob

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+     public function login(Request $request)
+    {
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password]))
+        {
+            $user = Auth::user();
+            /** @var \App\Models\User $user **/
+            $token = $user->createToken('app')->plainTextToken;
+            $arr = array('acceso' => "Ok",
+                        'error' => "",
+                        'token' => $token,
+                        'idUsuario' => $user->id,
+                        'nombreUsuario' => $user->name
+                        );
+   
+            return json_encode($arr);
+        }
+        else{
+   
+            $arr = array('acceso' => "",'token' => "",
+                        'error' => "No existe el usuario y/o contraseña",
+                        'idUsuario' => 0,
+                        'nombreUsuario' => '');
+            return json_encode($arr);
+        }
+    }
+}
